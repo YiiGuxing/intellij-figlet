9a5799fbd5c0897da3de1807e6beb010c17df98b
Yii.Guxing <yii.guxing@gmail.com>
2019-02-20 15:25:17 +0800

    Insert the generated ASCII art text into the editor

--- src/main/kotlin/cn/yiiguxing/plugin/figlet/GenerateASCIIArtAction.kt
+++ src/main/kotlin/cn/yiiguxing/plugin/figlet/GenerateASCIIArtAction.kt
@@ -1,12 +1,29 @@
 package cn.yiiguxing.plugin.figlet
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.codeInsight.editorActions.PasteHandler
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.editor.Caret
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.actionSystem.EditorAction
+import com.intellij.util.Producer
+import com.intellij.util.ui.TextTransferable
+import java.awt.datatransfer.Transferable
 
-class GenerateASCIIArtAction : AnAction() {
-    override fun actionPerformed(e: AnActionEvent) {
-        GenerateASCIIArtDialog(e.project!!).showAndGetResult()?.let {
-            println(it)
+
+class GenerateASCIIArtAction : EditorAction(GenerateASCIIArtHandler()) {
+
+    private class GenerateASCIIArtHandler : PasteHandler(null) {
+
+        override fun isEnabledForCaret(editor: Editor, caret: Caret, dataContext: DataContext?): Boolean {
+            return !editor.isViewer && editor.document.isWritable
+        }
+
+        override fun doExecute(editor: Editor, caret: Caret?, dataContext: DataContext?) {
+            val project = editor.project ?: return
+            val selectedText = editor.selectionModel.selectedText ?: ""
+            val asciiArtText = GenerateASCIIArtDialog(project, selectedText).showAndGetResult() ?: return
+
+            execute(editor, dataContext, Producer<Transferable> { TextTransferable(asciiArtText) })
         }
     }
 }
