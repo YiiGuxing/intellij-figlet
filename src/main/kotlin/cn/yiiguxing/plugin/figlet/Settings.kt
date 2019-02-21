4b5f22286819854a2fb668e970cf2db59587539e
Yii.Guxing <yii.guxing@gmail.com>
2019-02-21 10:57:44 +0800

    Add plugin settings

---
+++ src/main/kotlin/cn/yiiguxing/plugin/figlet/Settings.kt
@@ -0,0 +1,30 @@
+package cn.yiiguxing.plugin.figlet
+
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+
+@State(name = "Settings", storages = [Storage("yiiguxing.figlet.xml")])
+class Settings : PersistentStateComponent<Settings> {
+
+    var trimOutput: Boolean = true
+
+    var previewFontSize: Int = 12
+        get() = maxOf(field, 1)
+        set(value) {
+            field = maxOf(value, 1)
+        }
+
+    override fun getState(): Settings = this
+
+    override fun loadState(state: Settings) {
+        trimOutput = state.trimOutput
+        previewFontSize = state.previewFontSize
+    }
+
+    companion object {
+        val instance: Settings
+            get() = ServiceManager.getService(Settings::class.java)
+    }
+}
